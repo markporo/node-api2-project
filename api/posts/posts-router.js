4c68f65dd717b24b350943ee69e50cc3f260a17b
markporo <markporo@yahoo.com>
2021-10-30 15:45:16 -0400

    exporting post routes to server, routes written for get posts, get posts with id, and delete posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,46 @@
 // implement your posts router here
+const router = require('express').Router()
+const posts = require('./posts-model')
+
+// #### 1 [GET] /api/posts
+router.get('/', (req, res) => {
+    posts
+        .find()
+        .then((postsRecieved) => {
+            // if (!postsRecieved) return res.status(404).json({ Message: "No Posts Found" })
+            res.status(200).json(postsRecieved)
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The posts information could not be retrieved" })
+        })
+})
+
+
+// #### 2 [GET] /api/posts/:id
+router.get('/:id', async (req, res) => {
+    try {
+        const specifiedPost = await posts.findById(req.params.id)
+        if (!specifiedPost) return res.status(404).json({ message: "The post with the specified ID does not exist" })
+        res.status(200).json(specifiedPost)
+    } catch {
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    }
+})
+
+// #### 5 [DELETE] /api/posts/:id
+router.delete('/:id', async (req, res) => {
+    let id = req.params.id
+    try {
+        const postToBeDeleted = await posts.findById(id)
+        if (postToBeDeleted) {
+            await posts.remove(id) // don't forget the await keyword
+            res.status(200).json(postToBeDeleted)
+        }
+        if (!postToBeDeleted) return res.status(404).json({ message: "The post with the specified ID does not exist" })
+    } catch {
+        res.status(500).json({ message: "The post could not be removed" })
+    }
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,32 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express")
+const server = express()
+const helmet = require('helmet')
+const cors = require('cors')
+const morgan = require('morgan')
+
+
+const postsRouter = require('./posts/posts-router')
+
+//middleware
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(cors())
+server.use(express.json())
+
+//routes ---- don't put . in front of / on route directory
+server.use('/api/posts', postsRouter)
+
+//get home
+server.get('/', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: 'API is running: Wow what a nice Blog',
+        Date: new Date().toLocaleDateString(),
+    })
+})
+
+
+
+module.exports = server
